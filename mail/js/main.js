27fd538f78340484b98161528aecf198d177da9f
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-01-18 15:18:45 +0100

    feat: add array and prompt

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,1 +1,13 @@
 console.log('JS OK')
+
+// creo array
+const autorizedMails = ["tiziano@boolean.it", "mattia@boolean.it", "luigi@boolean.it"]
+console.log("lista mail accettate: " + autorizedMails)
+
+const mails = autorizedMails
+console.log(mails)
+
+// creo prompt input mail
+const inputMail = prompt('Inserisci la tua mail:')
+console.log("la mail inserita nel prompt è: " + inputMail)
+
